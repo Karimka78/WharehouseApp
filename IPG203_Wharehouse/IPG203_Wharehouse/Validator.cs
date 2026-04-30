615d9fe977e9e92f461befe496caeff8bb831ae4
mbrayez <mbrayez@gmail.com>
2026-04-30 20:44:55 +0300

    Order and Validator Classes

---
+++ IPG203_Wharehouse/IPG203_Wharehouse/Validator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPG203_Wharehouse
+{
+    public static class Validator
+    {
+        public static bool IsValidQuantity(int quantity)
+        {
+            return quantity > 0;
+        }
+
+        public static bool IsValidPrice(double price)
+        {
+            return price > 0;
+        }
+
+        public static bool IsValidName(string name)
+        {
+            return !string.IsNullOrWhiteSpace(name);
+        }
+
+        public static bool IsValidDiscount(double discount)
+        {
+            return discount >= 0 && discount <= 100;
+        }
+    }
+}
